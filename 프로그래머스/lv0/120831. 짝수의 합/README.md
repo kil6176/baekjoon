822a2c79f589029765f9631843a901fef56e5228
dkssud <71474589+kil6176@users.noreply.github.com>
2023-01-19 23:28:36 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/README.md
+++ 프로그래머스/lv0/120831. 짝수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수의 합 - 120831 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120831) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120831?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 0.02 ms
+메모리: 70.8 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,10 @@
 class Solution {
-        public static int solution(int n) {
-            int answer = 0;
+    public static int solution(int n) {
+        int answer = 0;
 
-            for(int i=0; i<=n; i++) answer += (i % 2 == 0) ? i : 0;
+        for(int i=0; i<=n; i++) 
+            answer += (i % 2 == 0) ? i : 0;
 
-            return answer;
-        }
+        return answer;
+    }
 }
