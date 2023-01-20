74b3e79264ef141fe3f2a4a425ffa07d9163acc1
dkssud <71474589+kil6176@users.noreply.github.com>
2023-01-20 21:04:17 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        int temp = 0;
+        for(int i = 0; i < num_list.length/2;i++){
+            temp = num_list[i];
+            num_list[i] = num_list[num_list.length - i - 1];
+            num_list[num_list.length - i - 1] = temp;
+        }
+        answer = num_list;
+        return answer;
+    }
+}
