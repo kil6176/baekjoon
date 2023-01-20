2e7303dc1888c4f808460c1ad3b85bf75a1752fa
dkssud <71474589+kil6176@users.noreply.github.com>
2023-01-20 21:00:27 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int n, int k) {
-        int kkochi = 12000;
-        int drink = 2000;
+        int kkochiPrice = 12000;
+        int drinkPrice = 2000;
         int drinkBonus = n/10; 
-        int kkochiMoney = n * kkochi;
-        int drinkMoney = (k - drinkBonus) * drink;
+        int totalKkochiPrice = n * kkochiPrice;
+        int totalDrinkPrice = (k - drinkBonus) * drinkPrice;
         
-        int answer = kkochiMoney + drinkMoney;
+        int answer = totalKkochiPrice + totalDrinkPrice;
         return answer;
     }
 }
