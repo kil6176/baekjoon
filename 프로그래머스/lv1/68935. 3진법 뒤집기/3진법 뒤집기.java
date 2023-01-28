3b94c0c308106a7f303aa13e09699aaaf5882f83
dkssud <71474589+kil6176@users.noreply.github.com>
2023-01-28 11:06:28 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        StringBuffer sb = new StringBuffer(Integer.toString(n,3));
-        answer = Integer.parseInt(sb.reverse().toString(),3);
+        answer = Integer.parseInt(new StringBuffer(Integer.toString(n,3)).reverse().toString(),3);
         return answer;
     }
 }
