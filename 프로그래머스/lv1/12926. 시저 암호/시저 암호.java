6f08469e0f3872fb05d4729ebaa42404f5189064
dkssud <71474589+kil6176@users.noreply.github.com>
2023-01-31 14:40:41 +0900

    [level 1] Title: 시저 암호, Time: 31.80 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -4,10 +4,10 @@ class Solution {
         String answer = "";
         for (int i = 0; i < s.length(); i++) {
             if(s.charAt(i) <= 'Z' &&  'Z' <  s.charAt(i) + n)
-                answer+= (char) ('A' - 1 + s.charAt(i) + n -'Z');
+                answer+= (char) ('A' + s.charAt(i) + n -'Z' - 1);
             
             else if('z' < s.charAt(i)+n)
-                answer+= (char) ('a' - 1 + s.charAt(i) + n -'z');
+                answer+= (char) ('a' + s.charAt(i) + n -'z' -1);
             
             else if(s.charAt(i) == ' ')
                 answer += String.valueOf(s.charAt(i));
