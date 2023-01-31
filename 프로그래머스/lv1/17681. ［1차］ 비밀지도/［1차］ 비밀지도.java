c7b76ab9e59589ab590ef7b840ddf94f2c029bd6
dkssud <71474589+kil6176@users.noreply.github.com>
2023-01-31 20:32:50 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.18 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,17 +1,6 @@
-import java.util.*;
 class Solution {
-    public static String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        for (int i = 0; i < n; i++) {
-            answer[i] = toBinary(n, arr1[i] | arr2[i]);
-            answer[i] = answer[i].replaceAll("1", "#");
-            answer[i] = answer[i].replaceAll("0", " ");
-        }
-        System.out.println(Arrays.toString(answer));
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
         return answer;
-    }
-
-    public static String toBinary(int n, int orNum) {
-        return String.format("%" + n + "s", Integer.toBinaryString(orNum)).replaceAll(" ", "0");
     }
 }
