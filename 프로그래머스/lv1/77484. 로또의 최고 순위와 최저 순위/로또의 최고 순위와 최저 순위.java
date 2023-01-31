6f1bd17e734a9423afb744ed94e8a8761be25453
dkssud <71474589+kil6176@users.noreply.github.com>
2023-01-31 14:46:48 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 1.84 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -5,13 +5,16 @@ class Solution {
         int []rank = {6,6,5,4,3,2,1};
         int unknownCount = 0;
         int winCount = 0;
+        //2중 for문
         for(int i=0; i<lottos.length; i++){
             for(int j=0; j<lottos.length; j++) {
+                //로또번호가 0이라면
                 if (lottos[i] == 0) {
                     unknownCount++;
                     winCount++;
                     break;
                 }
+                //로또번호가 당첨번호랑 같다면
                 if (lottos[i] == win_nums[j]) {
                     winCount++;
                     break;
