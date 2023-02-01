d796c358520f92d298e6137de3100453d02b8134
dkssud <71474589+kil6176@users.noreply.github.com>
2023-02-01 16:11:55 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class Solution {
-       public static int solution(int n, int[] lost, int[] reserve) {
+    public static int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        boolean[] arr = new boolean[n+2]; // 체육복 가지고있는사람
-        boolean[] reserveArr = new boolean[n+2]; //여벌배열
+        boolean[] arr = new boolean[n+1]; // 체육복 가지고있는사람
+        boolean[] reserveArr = new boolean[n+1]; //여벌배열
         ArrayList<Integer> arrayList = new ArrayList<>();
         // 일단 전체 true
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 1; i < arr.length; i++) {
             arr[i] = true;
         }
 
@@ -16,7 +16,7 @@ class Solution {
         }
 
 
-        //체육복 여벌은 있는데 잃어버린애들 true로 바꿔주고 여벌 없애기
+        //체육복 여벌 있는애들 체크하고 잃어버린애들 true로 바꿔주고 여벌 없애기
         for (int i = 0; i < reserve.length; i++) {
             arrayList.add(reserve[i]);
             if(arr[reserve[i]] == false){
@@ -24,21 +24,23 @@ class Solution {
                 arr[reserve[i]] = true;
             }
         }
-
+        
+        //체육복도 있고 여벌도 있는애들 true로 바꾸기
         for (int num : arrayList) {
             reserveArr[num] = true;
         }
 
-        arr[0] = false;
         //잃어버린애들 여벌 찾아주기
-        for (int i = 1; i < n+1; i++) {
+        for (int i = 1; i <= n; i++) {
             if (arr[i] == false) {
                 if (i != 0 && reserveArr[i - 1] == true) {
                     arr[i] = true;
                     reserveArr[i - 1] = false;
-                } else if (reserveArr[i + 1] == true) {
-                    arr[i] = true;
-                    reserveArr[i + 1] = false;
+                } else if ((i < n)) {
+                    if(reserveArr[i + 1] == true) {
+                        arr[i] = true;
+                        reserveArr[i + 1] = false;
+                    }
                 }
             }
         }
@@ -49,5 +51,5 @@ class Solution {
                 answer++;
         }
         return answer;
-       }
+    }
 }
