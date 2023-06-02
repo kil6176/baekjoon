25b36b60836eeac38350564c66a8125b92a46970
dkssud <71474589+kil6176@users.noreply.github.com>
2023-06-02 19:20:43 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,82 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void solve(int testcase) throws IOException {
+        int divideNum = testcase / 4;
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < divideNum; i++){
+            answer.append("long ");
+        }
+        answer.append("int");
+        Printer.println(answer);
+    }
+
+    public static void solve() throws IOException {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        int T = Reader.nextInt();
+        solve(T);
+        Printer.close();
+    }
+
+
+    static class Reader {
+        public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        public static StringTokenizer st = new StringTokenizer("");
+
+        public static boolean hasNextLine() throws IOException {
+            return in.ready();
+        }
+
+        public static String nextLine() throws IOException {
+            st = new StringTokenizer("");
+            return in.readLine();
+        }
+
+        public static String next() throws IOException {
+            while (!st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public static long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+
+    static class Printer {
+        public static PrintWriter out = new PrintWriter(System.out);
+
+        public static void println() {
+            out.println();
+        }
+
+        public static void println(Object object) {
+            out.println(object);
+        }
+
+        public static void print(Object object) {
+            out.print(object);
+        }
+
+        public static void close() throws IOException {
+            out.close();
+        }
+    }
+}
