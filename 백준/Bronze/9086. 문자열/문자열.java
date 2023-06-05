7b4314a7d9f5c00c0c157bfc4eead19ab10a9366
dkssud <71474589+kil6176@users.noreply.github.com>
2023-06-05 21:27:28 +0900

    [Bronze V] Title: 문자열, Time: 72 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,81 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void solve(int loopCount) throws IOException {
+        for(int i = 0 ; i < loopCount; i++){
+            String testcase = Reader.nextLine();
+            StringBuilder str = new StringBuilder();
+            str.append("" + testcase.charAt(0) + testcase.charAt(testcase.length() - 1));
+            Printer.println(str);
+        }
+    }
+
+    public static void solve() throws IOException {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        int T = Reader.nextInt();
+        solve(T);
+        Printer.close();
+    }
+
+
+    static class Reader {
+        public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        public static StringTokenizer st = new StringTokenizer("");
+
+        public static boolean hasNextLine() throws IOException {
+            return in.ready();
+        }
+
+        public static String nextLine() throws IOException {
+            st = new StringTokenizer("");
+            return in.readLine();
+        }
+
+        public static String next() throws IOException {
+            while (!st.hasMoreTokens()) {
+                st = new StringTokenizer(in.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public static int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public static long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public static double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+
+    static class Printer {
+        public static PrintWriter out = new PrintWriter(System.out);
+
+        public static void println() {
+            out.println();
+        }
+
+        public static void println(Object object) {
+            out.println(object);
+        }
+
+        public static void print(Object object) {
+            out.print(object);
+        }
+
+        public static void close() throws IOException {
+            out.close();
+        }
+    }
+}
